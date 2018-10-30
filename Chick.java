9baa5eb77e198cd12a7ad05eed057e80457b3b0b
JUSTIN GUAN <JUGUAN7@sfusd.edu>
2018-10-30 13:13:13 -0700

    OldMacDonald Program

--- Chick.java
+++ Chick.java
@@ -1,11 +1,21 @@
 class Chick implements Animal 
 {     
      private String myType;     
-     private String mySound;      
-     public Chick(String type, String sound)    
-     {         
-         myType = type;         
-         mySound = sound;     
+     private String mySound;  
+     private int a;    
+     public Chick(String type, String sound, String sound2)    
+     {   
+         a=(int)(Math.random()*2);      
+         myType = type; 
+         if (a==1)
+         {
+            mySound=sound;
+         } 
+         else
+         {
+            mySound=sound2;
+         }       
+          
      }     
      public Chick()     {         
          myType = "unknown";         
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -2,6 +2,4 @@ public void setup()
 {     
 	Farm a= new Farm();
 	a.animalSounds();
-    Cow c = new Cow("cow", "moo");   
-    System.out.println(c.getType() + " goes " + c.getSound());    
 }
